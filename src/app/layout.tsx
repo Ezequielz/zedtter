612d4fd09ee774b4d4f1f1fe7b49458ff982651d
Ezequiel <zapata.ed1989@gmail.com>
2023-08-18 19:48:41 -0300

    nextui implement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Providers } from './providers'
 
 export const metadata: Metadata = {
   title: 'Zedtter',
@@ -12,8 +13,10 @@ export default function RootLayout ({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body >{children}</body>
+    <html lang="en" className='dark'>
+      <Providers>
+        <body >{children}</body>
+      </Providers>
     </html>
   )
 }
