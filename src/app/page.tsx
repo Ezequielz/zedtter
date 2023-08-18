40f8399a3656a1a49182ae757877e8e43639f0f3
Ezequiel <zapata.ed1989@gmail.com>
2023-08-18 19:13:03 -0300

    supabase info user public

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { AuthButtonServer } from './components/auth-button-server'
 
 export default async function Home () {
   const supabase = createServerComponentClient({ cookies })
-  const { data: posts } = await supabase.from('posts').select('*')
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (session === null) {
+    redirect('/login')
+  }
+  const { data: posts } = await supabase
+    .from('posts')
+    .select('*, users(*)')
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
