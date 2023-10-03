8797e3b77cc24676b9455ee8fead52f69cdbae2a
Ezequiel <zapata.ed1989@gmail.com>
2023-10-03 00:25:23 -0300

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,20 @@ export default function RootLayout ({
   return (
     <html lang="en" className='dark'>
       <body >
+
         <Providers>
           {children}
+
         </Providers>
+        <footer className='relative flex justify-center items-center p-4'>
+          <span className='text-slate-100 absolute top-0'>Desarrollado por
+            <a className='ml-1 text-red-600 hover:text-red-700'
+              href="https://zapataezequiel.netlify.app/" target='_blank' rel='noreferrer'>
+              Ezequiel Zapata
+            </a>
+          </span>
+        </footer>
+
       </body>
     </html>
   )
